2df479220a884de6bf351780deb5bde92cfd37b3
baltasarq <baltasarq@gmail.com>
2017-10-30 11:09:22 +0100

    Traducir el mensaje

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace EmpezandoConGit
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hola, mundo!");
         }
     }
 }
